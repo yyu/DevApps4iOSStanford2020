c5b3287678e13f1dc7d0402d09356ed487e0d9ac
y² <yy@yuyuan.org>
2020-07-25 01:14:53 -0700

    New Xcode project.

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Yuan Yu on 7/25/20.
+//  Copyright © 2020 YY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
