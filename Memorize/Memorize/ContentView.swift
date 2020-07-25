8ab2e7929af8024316cf13927bb76571281ae6d2
y² <yy@yuyuan.org>
2020-07-25 02:32:57 -0700

    Simplify even more

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack() {
+        HStack {
             ForEach(0..<4) { index in
-                ZStack() {
+                ZStack {
                     RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
                     RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
                     Text("👻")
