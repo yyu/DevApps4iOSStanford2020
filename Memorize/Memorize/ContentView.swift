129fffc8ae7ba9bda013ddf35410df3731b94d7f
y² <yy@yuyuan.org>
2020-07-25 02:38:54 -0700

    Condition in ZStack

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         HStack {
             ForEach(0..<4) { index in
-                CardView()
+                CardView(isFaceUp: false)
             }
         }
             .padding()
@@ -22,11 +22,17 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    var isFaceUp: Bool
+
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-            Text("👻")
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
         }
     }
 }
