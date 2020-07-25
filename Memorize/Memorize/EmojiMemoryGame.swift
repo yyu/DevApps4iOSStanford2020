253f70bf7e9bcb3c0eb453db24399418c2c1d349
y² <yy@yuyuan.org>
2020-07-25 15:46:04 -0700

    MemoryGame<CardContent>; Add EmojiMemoryGame.swift

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,9 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Yuan Yu on 7/25/20.
+//  Copyright © 2020 YY. All rights reserved.
+//
+
+import Foundation
--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
