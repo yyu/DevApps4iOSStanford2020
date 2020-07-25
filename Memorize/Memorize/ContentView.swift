9e3c658c576036743a0d268bdd52b3f9ff9bfe91
y² <yy@yuyuan.org>
2020-07-25 02:07:15 -0700

    Use ZStack.foregroundColor

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -11,9 +11,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         return ZStack(content: {
-            RoundedRectangle(cornerRadius: 10.0).stroke().foregroundColor(Color.orange)
+            RoundedRectangle(cornerRadius: 10.0).stroke()
             Text("👻")
         })
+            .foregroundColor(Color.orange)
     }
 }
 
