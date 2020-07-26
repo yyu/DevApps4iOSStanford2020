2d606458cdd79bffd4170da0577659c2edee91e0
y² <yy@yuyuan.org>
2020-07-25 21:29:47 -0700

    Start writing EmojiMemoryGame

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2020 YY. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> =
+        MemoryGame<String>(numberOfPairsOfCards: 2, cardContentFactory: { (pairIndex: Int) -> String in
+            return "😀"
+        })
+
+    // MARK: - Access to the Model
+
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+
+    // MARK: - Intent(s)
+
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
