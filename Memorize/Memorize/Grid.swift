d0395e8aeacbb13edcbdfb5f2b4d26ce54c47b8f
y² <yy@yuyuan.org>
2020-07-27 01:33:50 -0700

    Create Grid.swift

---
+++ Memorize/Memorize/Grid.swift
@@ -0,0 +1,21 @@
+//
+//  Grid.swift
+//  Memorize
+//
+//  Created by Yuan Yu on 7/27/20.
+//  Copyright © 2020 YY. All rights reserved.
+//
+
+import SwiftUI
+
+struct Grid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Grid_Previews: PreviewProvider {
+    static var previews: some View {
+        Grid()
+    }
+}
