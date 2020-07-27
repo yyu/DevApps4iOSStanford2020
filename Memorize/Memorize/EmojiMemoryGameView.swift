c80832f23ff45de81995f2ce2f2ec2d19850141f
y² <yy@yuyuan.org>
2020-07-27 00:59:11 -0700

    Use constants instead of magic numbers

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -31,16 +31,22 @@ struct CardView: View {
         GeometryReader { geometry in
             ZStack {
                 if self.card.isFaceUp {
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                    RoundedRectangle(cornerRadius: self.cornerRadius).fill(Color.white)
+                    RoundedRectangle(cornerRadius: self.cornerRadius).stroke(lineWidth: self.edgeLineWidth)
                     Text(self.card.content)
                 } else {
-                    RoundedRectangle(cornerRadius: 10.0).fill()
+                    RoundedRectangle(cornerRadius: self.cornerRadius).fill()
                 }
             }
-            .font(Font.system(size: min(geometry.size.width, geometry.size.height) * 0.75))
+            .font(Font.system(size: min(geometry.size.width, geometry.size.height) * self.fontScaleFactor))
         }
     }
+
+    // MARK: - Drawing Constants
+
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    let fontScaleFactor: CGFloat = 0.75
 }
 
 
