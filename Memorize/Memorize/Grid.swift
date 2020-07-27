a42f50964f673d44e12aef56bc8d0fb81a255657
y² <yy@yuyuan.org>
2020-07-27 01:35:04 -0700

    Remove Grid_Previews

--- Memorize/Memorize/Grid.swift
+++ Memorize/Memorize/Grid.swift
@@ -13,9 +13,3 @@ struct Grid: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
-
-struct Grid_Previews: PreviewProvider {
-    static var previews: some View {
-        Grid()
-    }
-}
