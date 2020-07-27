77c12bc151c2de00ef76e5c74d7ecb2a36488348
y² <yy@yuyuan.org>
2020-07-27 01:12:10 -0700

    Move ZStack out of GeometryReader into a func to avoid typing self. all the time

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -29,24 +29,31 @@ struct CardView: View {
 
     var body: some View {
         GeometryReader { geometry in
-            ZStack {
-                if self.card.isFaceUp {
-                    RoundedRectangle(cornerRadius: self.cornerRadius).fill(Color.white)
-                    RoundedRectangle(cornerRadius: self.cornerRadius).stroke(lineWidth: self.edgeLineWidth)
-                    Text(self.card.content)
-                } else {
-                    RoundedRectangle(cornerRadius: self.cornerRadius).fill()
-                }
-            }
-            .font(Font.system(size: min(geometry.size.width, geometry.size.height) * self.fontScaleFactor))
+            self.body(for: geometry.size)
         }
+    }
+
+    func body(for size: CGSize) -> some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+            }
+        }
+        .font(Font.system(size: fontSize(for: size)))
     }
 
     // MARK: - Drawing Constants
 
     let cornerRadius: CGFloat = 10.0
     let edgeLineWidth: CGFloat = 3
-    let fontScaleFactor: CGFloat = 0.75
+
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
+    }
 }
 
 
