4f2aef9027218f6592b74b0c94b53837c63a2175
y² <yy@yuyuan.org>
2020-07-28 01:35:22 -0700

    Use GeometryReader in Grid

--- Memorize/Memorize/Grid.swift
+++ Memorize/Memorize/Grid.swift
@@ -18,8 +18,18 @@ struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     }
 
     var body: some View {
-        ForEach(items) {item in
-            self.viewForItem(item)
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
         }
+    }
+
+    func body(for size: CGSize) -> some View {
+        ForEach(items) { item in
+            self.body(for: item, in: size)
+        }
+    }
+
+    func body(for item: Item, in size: CGSize) -> some View {
+        viewForItem(item)
     }
 }
