fff138816506dab6d870f6b0608da72796312201
y² <yy@yuyuan.org>
2020-07-28 01:54:55 -0700

    Add a little more padding around cards

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -12,15 +12,14 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
 
     var body: some View {
-        HStack {
-            Grid(viewModel.cards) { card in
-                CardView(card: card).onTapGesture {
-                    self.viewModel.choose(card: card)
-                }
+        Grid(viewModel.cards) { card in
+            CardView(card: card).onTapGesture {
+                self.viewModel.choose(card: card)
             }
+            .padding(5)
         }
-            .padding()
-            .foregroundColor(Color.orange)
+        .padding()
+        .foregroundColor(Color.orange)
     }
 }
 
