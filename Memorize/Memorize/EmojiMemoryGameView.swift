ec28a1ae5074246c1a7cdc8f9474d879651eec0c
y² <yy@yuyuan.org>
2020-07-30 01:35:57 -0700

    Make many func/attributes private

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -32,7 +32,7 @@ struct CardView: View {
         }
     }
 
-    func body(for size: CGSize) -> some View {
+    private func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
@@ -49,10 +49,10 @@ struct CardView: View {
 
     // MARK: - Drawing Constants
 
-    let cornerRadius: CGFloat = 10.0
-    let edgeLineWidth: CGFloat = 3
+    private let cornerRadius: CGFloat = 10.0
+    private let edgeLineWidth: CGFloat = 3
 
-    func fontSize(for size: CGSize) -> CGFloat {
+    private func fontSize(for size: CGSize) -> CGFloat {
         min(size.width, size.height) * 0.75
     }
 }
