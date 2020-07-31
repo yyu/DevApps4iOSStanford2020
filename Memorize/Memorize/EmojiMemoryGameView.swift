cbf5d32265a9bae04d1a0c8850edabde8de62c6d
y² <yy@yuyuan.org>
2020-07-31 01:00:21 -0700

    Introduce Pie

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -37,6 +37,8 @@ struct CardView: View {
             if card.isFaceUp {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                    .padding(5).opacity(0.4)
                 Text(card.content)
             } else {
                 if !card.isMatched {
@@ -53,7 +55,7 @@ struct CardView: View {
     private let edgeLineWidth: CGFloat = 3
 
     private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
 }
 
