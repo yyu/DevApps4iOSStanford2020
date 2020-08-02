2bd62741708c5df5b620e8c03ca5a88a634211e2
y² <yy@yuyuan.org>
2020-08-01 19:43:13 -0700

    Matched emoji rotates

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -40,6 +40,8 @@ struct CardView: View {
                     .padding(5).opacity(0.4)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(Animation.linear(duration: 1))
             }
             .cardify(isFaceUp: card.isFaceUp)
         }
